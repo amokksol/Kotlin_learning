77420fa5ef9b151c1c5353501df9bf94c309651b
kostia18 <amokksol@gmail.com>
2020-11-25 17:05:46 +0300

    Работа с супер-классами подклассами и наследование функции

---
+++ src/Chapter5_Animals.kt
@@ -0,0 +1,60 @@
+//создание класса животных
+//OPEN если ставим перед классом - определяем его как супер класс
+//Если OPEN  перед свойствами класса - то значит эти свойства должны переопределяться
+open class Animal {
+    open val image=""
+    open val food=""
+    open val habitat=""
+    val hunger=10
+    //OPEN перед функциями - указывает, что они будут переопределяться для каждого животного возможно по разному
+    //звук издаваемый животным
+    open fun makeNoise(){
+        println("Животное издает свой звук")
+    }
+    //пища
+    open fun eat(){
+        println("Животное употребляет пищу")
+    }
+    //ореал обитания
+    open fun roam() {
+        println("У животного есть Вид")
+    }
+    //сон
+    fun sleep() {
+        println("Живтное может спать")
+    }
+}
+//создаем подкласс гиппо - и указываем его наследование от суперкласса Animal
+class Hippo: Animal(){
+    //переопределяем свойства которые наследуем от суперкласса Animals
+    override val image = "hippo.jpg"
+    override val food  = "Трава"
+    override val habitat = "Вода"
+    override fun makeNoise() {
+        println("Фыр Фыр Фыр")
+    }
+    override fun eat(){
+        println("Гиппопотам ест  $food")
+    }
+}
+open class Canine: Animal(){
+    override fun roam() {
+        println("Это вид - собачьих")
+    }
+}
+
+class Wolf: Canine(){
+    override val image = "wolf.jpg"
+    override val food  = "мясо"
+    override val habitat = "Леса"
+    override fun makeNoise() {
+        println("АУУУУУУУ")
+    }
+    override fun eat(){
+        print("Волки едят $food")
+    }
+}
+
+fun main() {
+
+}
