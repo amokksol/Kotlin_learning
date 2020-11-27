86f2eb826bd7a52260b5b40ea85c9dad924aa936
kostia18 <amokksol@gmail.com>
2020-11-27 12:16:55 +0300

    Конец работы с классами и полиформизмом, вызов функции main

--- src/Chapter5_Animals.kt
+++ src/Chapter5_Animals.kt
@@ -54,7 +54,23 @@ class Wolf: Canine(){
         print("Волки едят $food")
     }
 }
+class Vet {
+    fun giveShot(animal:Animal) {
+        animal.makeNoise()
+    }
+}
 
-fun main() {
+fun main(args:Array<String>) {
+    val animals= arrayOf(Hippo(), Wolf())
+    for (item in animals) {
+        item.roam()
+        item.eat()
+    }
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+
 
 }
