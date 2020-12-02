34aaeb5f51df8537473b50db03a0f4b0ac65f315
kostia18 <amokksol@gmail.com>
2020-12-02 14:23:22 +0300

    Общий вид перегруженных функций, классы с первчиными и вторичными конструктами

--- src/chapter7_Recipes.kt
+++ src/chapter7_Recipes.kt
@@ -1,8 +1,39 @@
-//объявляем класс данных не ставим  - {}  - так как класс не имеет тела
-data class Recipes(val title:String, val isVegetarian:Boolean)
+//объявляем класс указываем свойства, 2 свойства из 4 имеют значения по умолчанию false & easy
+data class Recipes(val title:String,
+                   val mainIngredient: String,
+                   val isVegetarian:Boolean = false,
+                   val difficulty: String = "Easy"){
+
+}
+
+//создаем для примера класс со вторичным  конструктором для примера
+class Mushroom(val size: Int, val isMagic:Boolean){
+  constructor(isMagic_param: Boolean) : this(0, isMagic_param){
+
+  }
+
+}
+//функция использующая значения параметров по умолчанию
+fun findRecepies(title: String ="",
+                 ingredient:String="",
+                 isVegetarian:Boolean=false,
+                 difficulty:String=""):Array<Recipes>
+{
+  //код поиска рецепта
+  return arrayOf(Recipes(title, ingredient,isVegetarian,difficulty))
+
+}
+
+//перегруженные функции
+fun addNumbers(a:Int,b:Int): Int {
+  return a+b
+}
+fun addNumbers(a:Double, b:Double):Double{
+  return a+b
+}
 fun main() {
-  val r1 = Recipes("Thai Curry", false)
-  val r2 = Recipes("Thai Curry", false)
+  val r1 = Recipes("Thai Curry", "Chicken")
+  val r2 = Recipes(title="Thai Curry", mainIngredient = "Chicken")
     //создаем переменную - копию r1 - с изменным параметром
   val r3 = r1.copy(title = "Chicken Dhuna")
     //выводим хэшируемые значения переменных
@@ -17,7 +48,17 @@ fun main() {
     println("r1 === r2? ${r1===r2}")
     println("r1 == r3? ${r1==r3}")
     //деструктуризация на отдельные перемнные
-    val (title, vegetarian) = r1
+    val (title, mainIngredient,vegetarian, difficulty) = r1
     println("$title,$vegetarian")
+  //создание объекта Mushroom вызовом первичного констурктора
+  val m1=Mushroom(6,false)
+  println("m1 размер: ${m1.size} он магичен?: ${m1.isMagic}")
+  //создание объекта вызовом вторичного конструктора
+  val m2 = Mushroom(true)
+  println("m2 размер: ${m2.size} он магичен?: ${m2.isMagic}")
+  //вызов перегруженных функций int и double
+  println(addNumbers(2,5))
+  println(addNumbers(1.6,7.3))
+
 
 }
